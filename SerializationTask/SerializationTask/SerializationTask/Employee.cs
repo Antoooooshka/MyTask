7344839eeef50019bbcd150579710b0b47232774
Anton <Antoooooshka@mail.ru>
2016-01-04 15:34:35 +0300

    Много переделать в ежедневнике

---
+++ SerializationTask/SerializationTask/SerializationTask/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace SerializationTask
+{
+    [Serializable]
+    public class Employee : IXmlSerializable
+    {
+        
+        public int Age { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Gender { get; set; }
+        
+        public Organization Organization { get; set; }
+
+        public Employee() { }
+
+        public Employee(int age, string name, string gender, Organization organiz)
+        {
+            Age = age;
+            Name = name;
+            Gender = gender;
+            Organization = organiz;
+        }
+
+        public System.Xml.Schema.XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(System.Xml.XmlReader reader)
+        {
+            reader.MoveToContent();
+        }
+
+        public void WriteXml(System.Xml.XmlWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SerializationTask/SerializationTask/SerializationTask/Services.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SerializationTask
+{
+    class Services
+    {
+
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+        XmlSerializer xs = new XmlSerializer(typeof(Employee));
+
+        public void XmlSerializerMethod(Employee emp, string xmlFileName)
+        {
+            using (Stream s = File.OpenWrite(xmlFileName))
+            {
+                xs.Serialize(s, emp);
+            }
+            Console.WriteLine("Объект сериализован");
+        }
+
+        public Employee XmlDeSerializerMethod(string xmlFilename)
+        {
+            Employee emp = null;
+
+            if (File.Exists(xmlFilename))
+            {
+                using (Stream s = File.OpenRead(xmlFilename))
+                {
+                    emp = (Employee)xs.Deserialize(s);
+                }
+            }
+            return emp;
+        }
+
+        public void BinnarySerializermethod(Employee employee, string fileName)
+        {
+
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                binaryFormatter.Serialize(fs, employee);
+            }
+            Console.WriteLine("Объъект сериализован");
+        }
+
+        public Employee BinnaryDeSerializerMethod(string fileName)
+        {
+            Employee emp;
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                emp = (Employee)binaryFormatter.Deserialize(fs);
+            }
+            return emp;
+        }
+
+    }
+}
