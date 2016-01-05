2b3dd7751265c6852975af46072cf01b05fbbdfc
Antoooooshka <Antoooooshka@Mail.ru>
2016-01-05 20:26:03 +0300

    проверка связи с компа

---
+++ SerializationTask/SerializationTask/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SerializationTask
+{
+    [Serializable]
+    public class Employee
+    {
+        
+        public int Age { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string Gender { get; set; }
+        
+        public Organization Organization { get; set; }
+
+        public Employee() { }
+
+        public Employee(int age, string name, string gender, Organization organiz)
+        {
+            Age = age;
+            Name = name;
+            Gender = gender;
+            Organization = organiz;
+        }
+    }
+}
---
+++ SerializationTask/SerializationTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace SerializationTask
+{
+    class Program
+    {
+        static void Main()
+        {
+            Organization artezio = new Organization("Artezio","cool adress");
+            Employee emp = new Employee(24, "Anton", "Male", artezio);
+            Services serv = new Services("Employee.xml");
+
+            Employee emp2 = new Employee();
+
+            serv.BinnarySerializermethod(emp);
+
+            emp2 = serv.BinnaryDeSerializerMethod();
+
+            Console.WriteLine(emp2.Name);
+            Console.WriteLine(emp2.Organization.OrganizationName);
+            Console.ReadLine();
+
+
+
+        }
+
+       
+
+       
+    }
+}
