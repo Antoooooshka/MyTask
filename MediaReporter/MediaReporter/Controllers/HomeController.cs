2a86e13ae4e9f855b2f0af37fd1395b569595cd2
Anton <Antoooooshka@mail.ru>
2016-07-27 19:17:52 +0300

    Проект, который ищет людей в вк рядом с красной площадью

---
+++ MediaReporter/MediaReporter/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MediaReporter.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult User()
+        {
+            //char[] separators = { '=', '&' };
+            //string[] responseContent = responseString.Split(separators);
+            //string accessToken = responseContent[1];
+            //int userId = Int32.Parse(responseContent[5]);
+            return View();
+        }
+
+        private void NearUsers(object[] items)
+        {
+        }
+
+    }
+}
---
+++ MediaReporter/MediaReporter/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MediaReporter.Models
+{
+    public class User
+    {
+        string user_id { get; set; }
+        string email { get; set; }
+    }
+}
